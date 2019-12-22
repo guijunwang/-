689fab518da6f9a552c0caa983eb68677ffac542
KSM <396585860@qqcom>
2019-12-22 14:54:50 +0800

    创建

---
+++ src/test/java/com/gjw/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
